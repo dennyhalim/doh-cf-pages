24926f2c361a809a72e87ba0adf2396f228fecbf
DennyHalim.com <mail2dny@gmail.com>
2026-01-14 10:58:50 +0700

    Update DoH URLs to new endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
-const doh = 'https://security.cloudflare-dns.com/dns-query'
-const dohjson = 'https://security.cloudflare-dns.com/dns-query'
+const doh = 'https://freedns.controld.com/no-ads-porn-dating-drugs-gambling-malware-typo'
+const dohjson = 'https://freedns.controld.com/no-ads-porn-dating-drugs-gambling-malware-typo'
 const contype = 'application/dns-message'
 const jstontype = 'application/dns-json'
 
